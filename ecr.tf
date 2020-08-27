d65cbcf91cffadc2e8308ae3b5dbccb4a4c09924
Tony Nyurkin <tony@simplepractice.com>
2020-08-27 13:28:20 +0300

    Add lifecycle policy

--- ecr.tf
+++ ecr.tf
@@ -6,3 +6,26 @@ resource "aws_ecr_repository" "image" {
     scan_on_push = local.scan
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "this" {
+  repository = aws_ecr_repository.image.name
+  policy     = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 7 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 7
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
